8245f346983e4afa68b50e6a117a81404b9571aa
SakshiSingh-NITBhopal <sakshisinghnitbhopal@gmail.com>
2025-06-09 22:41:51 +0530

    add add-task feature

--- src/index.js
+++ src/index.js
@@ -1,34 +1,43 @@
 document.addEventListener("DOMContentLoaded",()=>{
-    //reading local storage
-
-    //list of tasks
-    const to_do_tasks = [];
-
+    
+    //storing the tasks into array
+    const to_do_tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+    
     const plusIcon = document.getElementById("plus-icon");
     const inputBox = document.getElementById("input-box");
     const newTask = document.getElementById("new-task");
     const taskButton = document.getElementById("add-button");
     const taskWindow = document.getElementById("tasks-window");
-
+    
+    //iterating over to_do_tasks and displaying it
+    to_do_tasks.forEach(task => {
+        const li  = document.createElement("li");
+        li.innerHTML = 
+        `<input type="checkbox"> ${task}`;
+        taskWindow.appendChild(li);
+    });
+   
 
     plusIcon.addEventListener("click", ()=>{
         inputBox.classList.toggle("hidden");  
     })
 
     taskButton.addEventListener("click",()=>{
-        //grab input box value and append to "to_do_tasks"
-        inputBox.classList.toggle("hidden");
-        
         const li  = document.createElement("li");
         li.innerHTML = 
         `<input type="checkbox"> ${newTask.value}`;
         taskWindow.appendChild(li);
-        to_do_tasks.push(newTask.value);
-        console.log(to_do_tasks);
 
-        newTask.value = "";
+        //push new item to array and update localstorage
+        to_do_tasks.push(newTask.value);
+        localStorage.setItem("tasks",JSON.stringify(to_do_tasks));
+        
+        //hide the input box
+        inputBox.classList.toggle("hidden");
+        console.log(to_do_tasks);
 
     })
     
+
     
 });
