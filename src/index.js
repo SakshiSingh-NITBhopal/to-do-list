080556cb176b0bd9b2249a7025000991bc304549
SakshiSingh-NITBhopal <sakshisinghnitbhopal@gmail.com>
2025-06-12 19:02:08 +0530

    add delete feature

--- src/index.js
+++ src/index.js
@@ -1,13 +1,7 @@
 document.addEventListener("DOMContentLoaded",()=>{
-    
-    //storing the tasks into array
-    // {
-    // name:"drink water",
-    // isCompleted = true
-    // }
-    const to_do_tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+  
+    let to_do_tasks = JSON.parse(localStorage.getItem("tasks")) || [];
     console.log(to_do_tasks);
-    
     
     const plusIcon = document.getElementById("plus-icon");
     const inputBox = document.getElementById("input-box");
@@ -15,26 +9,22 @@ document.addEventListener("DOMContentLoaded",()=>{
     const taskButton = document.getElementById("add-button");
     const taskWindow = document.getElementById("tasks-window");
     const taskArea = document.getElementById("task-area");
-    
-    
+
     //iterating over to_do_tasks and displaying it
-    // for(const key )
-    let number =  0;
     to_do_tasks.forEach(task => {
         const li  = document.createElement("li");
         li.innerHTML = `
         ${task.name}
         <button class="delete-button">Delete</button>`;
-        li.setAttribute("data-number", `${number}`);
+        li.setAttribute("data-number", `${task.id}`);
         li.classList.add("to-do");
         taskWindow.appendChild(li);
         
         if(task.isCompleted === true) 
             { 
-                const target = document.querySelector(`[data-number="${number}"]`);
+                const target = document.querySelector(`[data-number="${task.id}"]`);
                 target.classList.add("strikethrough");
             }
-        number++;
     });
     
     
@@ -44,56 +34,62 @@ document.addEventListener("DOMContentLoaded",()=>{
     })
 
     taskButton.addEventListener("click",()=>{
+        //generating random number
+        let N = 100;
+        let randomNumber = Math.floor(Math.random() * N);
+
+        //grabbing input and adding into the dom as a alist
         const li  = document.createElement("li");
         li.innerHTML = `${newTask.value}
         <button class="delete-button">Delete</button>`;
         li.classList.add("to-do");
-        li.setAttribute("data-number", `${number}`);
+        li.setAttribute("data-number", `${randomNumber}`);
         taskWindow.appendChild(li);
 
         //push new item to array and update localstorage
         const newList =
         {
-            id : number,
+            id : randomNumber,
             name : newTask.value,
             isCompleted : false
         };
-        number++;
         to_do_tasks.push(newList);
         localStorage.setItem("tasks",JSON.stringify(to_do_tasks));
         
+        //erase the input box text and hide
         newTask.value = "";
-        //hide the input box
         inputBox.classList.toggle("hidden");
     })
     
     taskArea.addEventListener("click",(e)=>{ 
         if(e.target.tagName === "LI")
         {
-            e.target.classList.toggle("strikethrough");
-            
-            console.log(e.target);    
+            //when the user clicks on the list, it will get strike through
+            e.target.classList.toggle("strikethrough"); 
 
             //find the element in the array and toggle isCompleted value
             const taskId = e.target.getAttribute("data-number");
             const selectedTask = to_do_tasks.find(task => task.id === Number(taskId));
             
+            //updating to_do_tasks and local storage
             selectedTask.isCompleted = !(selectedTask.isCompleted);
             localStorage.setItem("tasks",JSON.stringify(to_do_tasks));
-            console.log(to_do_tasks);
             
         }
         //adding delete feature
-        else if(e.target.tagName === "BUTTON")
+        else if(e.target.tagName === "BUTTON" && e.target.getAttribute("class") === "delete-button")
         {
-            console.log("hi from delete");
             const deleteElement = e.target.closest(".to-do"); 
-            const deleteId = Number(e.target.closest(".to-do").getAttribute("data-number"));
+            const deleteId = Number(deleteElement.getAttribute("data-number"));
             
-            const updated_tasks = to_do_tasks.filter(task => task.id !== deleteId);
-            console.log(updated_tasks);
-            localStorage.setItem("tasks",JSON.stringify(updated_tasks));
+            //deleting the particular element from the array
+            to_do_tasks = to_do_tasks.filter(task => task.id !== deleteId);
+
+            //finding the particular element and updating into to_do_tasks and local storage
+            let deleteObject = to_do_tasks.find(task => task.id === deleteId);
+            localStorage.setItem("tasks",JSON.stringify(to_do_tasks));
             
+            //removing the particular element from the dom
             deleteElement.remove();
         }
         
